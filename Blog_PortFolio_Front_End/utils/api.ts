acfcce8b5a2f2909400e3d8e7241452bf73990cb
장효환 <jalsos777@naver.com>
2025-06-20 16:52:31 +0900

    프론트 배포중

--- Blog_PortFolio_Front_End/utils/api.ts
+++ Blog_PortFolio_Front_End/utils/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { Guestbook } from "@/types/Guestbook";
 
-const API_URL = "http://localhost:3001";
+// 환경변수를 사용하고, 기본값으로 localhost 제공
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
+// 전체 방명록 조회
 export const getGuestbooks = async (): Promise<Guestbook[]> => {
   try {
     const response = await axios.get(`${API_URL}/guestbooks`);
@@ -13,6 +15,7 @@ export const getGuestbooks = async (): Promise<Guestbook[]> => {
   }
 };
 
+// 단일 방명록 조회
 export const getGuestbook = async (
   id: string
 ): Promise<{
@@ -30,6 +33,7 @@ export const getGuestbook = async (
   }
 };
 
+// 방명록 작성
 export const createGuestbook = async (
   guestbook: Omit<Guestbook, "id" | "createdAt" | "likes">
 ): Promise<Guestbook> => {
@@ -42,6 +46,7 @@ export const createGuestbook = async (
   }
 };
 
+// 방명록 삭제
 export const deleteGuestbook = async (id: string): Promise<void> => {
   try {
     await axios.delete(`${API_URL}/guestbooks/${id}`);
